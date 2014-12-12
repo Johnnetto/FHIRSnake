858af71d30dc21edbfdca777511bac51021b5f97
Federico Milano <fmilano@gmail.com>
2014-12-12 00:48:02 -0300

    Resource class added.

---
+++ fhir/resource.py
@@ -0,0 +1,19 @@
+__author__ = 'Federico Milano'
+
+class Resource:
+    def __init__(self):
+        self.extension = []
+        self.modifierExtension = []
+        self.__language = ''
+        self.text = ''
+        self.contained = []
+
+    @property
+    def language(self):
+        return self.__language
+
+    @code.setter
+    def language(self, code):
+        if not isinstance(code, fhir.primitive.Code):
+            raise TypeError('code has to be a fhir.primitive.Code instance')ld.')
+        self.__language = code
